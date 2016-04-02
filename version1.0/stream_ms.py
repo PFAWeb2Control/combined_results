15fe8d6dbb421875061c1fa439334f4b1b7f56ff
irifki <rifkiimane@gmail.com>
2016-04-02 14:00:10 +0200

    Update and rename vectorize.py to stream_ms.py

--- version1.0/stream_ms.py
+++ version1.0/stream_ms.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-#corpus=['Rendez-vous au Boogie Spirit Festival ce weekend à Illkirch','Samedi 6 Février, à la ComedieFr pour voir "La Double inconstance" de Marivaux!','Toujours personne pour le 104paris ce soir ? Une pièce pleine d\'humour qui mêle théâtre et danse, ça ne vous dit pas ?','Pour les fans de réécritures de contes, ne manquez pas ‘The Forbidden Wish’ le 23 février prochain!']
 
 import time
 from tweepy_import import FilteredStream
@@ -19,6 +18,7 @@ class MyFilteredStream(FilteredStream):
     def __init__(self):
 
         self.criterias = {
+            #You can choose some key words to look for by chaging the track's content
             "track": ['remaniement', 'Paris'],
             "locations": [-0.6389644,44.8111222,-0.5334955,44.9163535],
             "lang": [*]
@@ -35,44 +35,32 @@ class MyFilteredStream(FilteredStream):
         vectorizer = CountVectorizer()
         X = vectorizer.fit_transform(corpus)
         M,P=X.shape
-
+        """
+        #De-comment this section for further informations about the corpus, and the dictionnary
         print("taille du corpus : ",M)
         print("taille du vocabulaire : ",P)
         print("Mon vocabulaire")
         print(vectorizer.vocabulary_)
         print("Mes vecteurs")
         print (X.toarray())
-
-        #On peut calculer la distance euclidienne entre ces différents messages
-
+        
+        """
+        #We can calculate the euclidian distance between the different messages
         print("La distance euclidienne entre les diffents messages:")
         dist_corpus=euclidean_distances(X)
         print(dist_corpus)
+        """
 
-        #on peut calculer la distance d'un nouveau message à ce  corpus
-        print("distance dun nouveau message non evenement a ce corpus")
-        msg="Luc Fortin fait son entrée au Cabinet des ministres. Il s’occupera du Loisir et du Sport"
-        y=vectorizer.transform([msg])
-        dist_to_y=euclidean_distances(y,X)
-        print(dist_to_y)
-
-        #on peut calculer la distance d'un nouveau message à ce  corpus
-        print("distance dun nouveau message evenement a ce corpus")
-        msg="Sortie ce soir"
-        y=vectorizer.transform([msg])
-        dist_to_y=euclidean_distances(y,X)
-        print(dist_to_y)
-
-        #on peut réduire la taille du vocabulaire
-        #suppresion des mots très usuels (stop words)
-        #utilisation de l'argument stop_words de la classe CountVectorizer
-
+        #Suppression of french stopwords
         stwf=stopwords.words('french')
         stwf.append('les')
         vectorizer=CountVectorizer(stop_words=stwf)
         X = vectorizer.fit_transform(corpus)
         dico=vectorizer.vocabulary_
         Ns=len(dico)
+        
+        """
+        #De-coment this section for further information about the dictionnar, the new array and the euclidian distance. 
         print("taille du vocabulaire apres suppression des stop_words : ",Ns)
         print("mon nouveau dictionnaire")
         print(dico)
@@ -81,10 +69,11 @@ class MyFilteredStream(FilteredStream):
         dist_corpus=euclidean_distances(X)
         print("ma nouvelle matrice de distance")
         print(dist_corpus)
-
+        """
+        #Apply the mean_shift algorithm on our new X after the suppression of the stopwords
         print("Résultats des l'algorithme Mean Shift")
         ms_algo(X.toarray(), None)
         quit()
-
+#Call the method define previously with the meanshift algorithm with the current stream of tweets
 stream = MyFilteredStream()
 stream.stream()
