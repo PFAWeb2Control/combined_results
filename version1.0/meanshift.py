8142f17e971e0991a8e45bf0c4152b9aeb9c9597
irifki <rifkiimane@gmail.com>
2016-04-02 13:28:08 +0200

    Update meanshift.py

--- version1.0/meanshift.py
+++ version1.0/meanshift.py
@@ -2,29 +2,35 @@ import numpy as np
 from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn.datasets.samples_generator import make_blobs
 
+"""
+ms_algo functions runs the meanshift algorithm
+Parameters: 
+-X[array] is the dataset array.
+-bandwith[float] optinal
+"""
 def ms_algo(X, bandwidth=None):
     if(bandwidth==None):
         n_samples = X.shape[0]
         bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=n_samples)
 
-    # Application de l'algorithme MeanShift
+    # Apply the meanshit algorithm from sklearn library
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
     ms.fit(X)
 
-    # Recuperation des etiquettes de chaque point et du centre de chaque regroupement
+    # collect from the meanshift algorithm the labels and the centers of the clusters
     labels = ms.labels_
     cluster_centers = ms.cluster_centers_
 
 
     labels_unique = np.unique(labels)
-    n_clusters_ = len(labels_unique) # Nombre de regroupements
+    n_clusters_ = len(labels_unique) #Number of clusters
 
-    # Affichage
+    # Print section
     print("Nombre de regroupement(s): %d" % n_clusters_)
 
     print("Centre(s):")
     for i in range(n_clusters_):
         print i,
         print cluster_centers[i]
-
+    #return the clusters's centers
     return cluster_centers    
